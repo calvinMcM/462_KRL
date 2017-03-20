3ac9ab0f88e7a1184ee26fb59735f590a3c8708a
calvinMcM <calvin.r.mcm@gmail.com>
2017-03-20 12:32:47 -0600

    2.3

---
+++ partI/trips-out.js
@@ -0,0 +1,57 @@
+module.exports = {
+  "rid": "track_trips",
+  "meta": {
+    "name": "Track Trips",
+    "description": "\nA first ruleset for the Quickstart\n",
+    "author": "Phil Windley",
+    "logging": true,
+    "shares": [
+      "message",
+      "__testing"
+    ]
+  },
+  "global": function* (ctx) {
+    ctx.scope.set("__testing", {
+      "queries": [],
+      "events": [{
+          "domain": "echo",
+          "type": "message",
+          "attrs": ["name"]
+        }]
+    });
+  },
+  "rules": {
+    "process_trip": {
+      "name": "process_trip",
+      "select": {
+        "graph": { "echo": { "message": { "expr_0": true } } },
+        "eventexprs": {
+          "expr_0": function* (ctx) {
+            return true;
+          }
+        },
+        "state_machine": {
+          "start": [[
+              "expr_0",
+              "end"
+            ]]
+        }
+      },
+      "prelude": function* (ctx) {
+        ctx.scope.set("milage", yield ctx.callKRLstdlib("klog", yield (yield ctx.modules.get(ctx, "event", "attr"))(ctx, ["milage"]), "our passed in mileage: "));
+      },
+      "action_block": {
+        "actions": [{
+            "action": function* (ctx) {
+              return {
+                "type": "directive",
+                "name": "trip",
+                "options": { "trip_length": ctx.scope.get("milage") }
+              };
+            }
+          }]
+      }
+    }
+  }
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbXX0=
